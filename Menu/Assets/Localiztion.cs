0fd98f6afd140243759250bad87650d3af1f673e
Akimax12 <mhuo544@aucklanduni.ac.nz>
2017-10-02 11:43:39 +1300

    changes

---
+++ Menu/Assets/Localiztion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Localiztion : MonoBehaviour {
+
+    // Use this for initialization
+    public Dropdown dropDown;
+    public Sprite[] modes;
+    private void Start()
+    {
+        dropDown.ClearOptions();
+
+        List<Dropdown.OptionData> menuItems = new List<Dropdown.OptionData>();
+
+        foreach(var mode in modes)
+        {
+            var modeOptions = new Dropdown.OptionData(mode.name, mode);
+            menuItems.Add(modeOptions);
+        }
+
+        dropDown.AddOptions(menuItems);
+    }
+}
